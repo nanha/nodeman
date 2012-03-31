b33f766433d453b225e245b9b531a7aa1ae07161
root <nanhap@gmail.com>
2012-03-31 09:45:00 +0900

    fixes public/native builtin check

---
+++ docs/public_test_doc.js
@@ -0,0 +1,5 @@
+exports.name = 'public test (Not node.js black edition)';
+exports.context = 'use require';
+exports.homepage = 'http://startic.kr/njs/package/...';
+exports.description = [
+].join('\n');
