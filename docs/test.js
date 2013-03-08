fb22ea26201f795e65a4ba0c4162827b82258276
nanha park <nanhap@gmail.com>
2013-03-08 22:31:44 +0900

    dummy

--- docs/test.js
+++
@@ -1,6 +0,0 @@
-exports.name = 'public test (Not node.js black edition)';
-exports.category = 'category';
-exports.context = 'use require';
-exports.homepage = 'http://startic.kr/njs/package/...';
-exports.description = [
-].join('\n');
